eec3b191d4b80d3581e526b25aba0296ecb0d395
pradyumna <newton@newtonflash.com>
2018-02-10 15:08:19 +0530

    added search

--- src/container/product-listing.js
+++ src/container/product-listing.js
@@ -10,16 +10,58 @@ export default class ProductListing extends React.Component{
         super(props);
 
         this.state = {
-
+            products: [ ],
+            searchString : ""
         }
+            this.searchHandler = this.searchHandler.bind(this);
+    }
+
+    componentDidMount(){
+        fetch("products.json")
+            .then((res)=>{
+                return res.json();
+            })
+            .then((data)=>{
+                this.setState({
+                    products : data.products
+                })
+            });
+    }
+
+    searchHandler (searchKey){
+        this.setState({
+            searchString:searchKey
+        })
+    }
+
+    createCardList () {
+        let searchString = this.state.searchString;
+        let filteredCards = this.state.products;
+
+        if(searchString !== ""){
+            filteredCards = this.state.products.filter((item) => {
+                return item.name == searchString;
+            });
+        }
+
+        let cards = filteredCards.map((item, index) => {
+                return (
+                    <ProductCard key={index} data = {item} ></ProductCard>
+                )
+        });
+        return cards;
+
     }
 
     render(){
+
+
+
         return(
             <Fragment>
                 <div className="row">
                     <div className="col-sm-10">
-                        <Search></Search>
+                        <Search searchHandler = {this.searchHandler}></Search>
                     </div>
                     <div className="col-sm-2">
                         <Sorting></Sorting>
@@ -31,7 +73,9 @@ export default class ProductListing extends React.Component{
                         <Filter></Filter>
                     </div>
                     <div className="col-sm-10">
-                        <ProductCard></ProductCard>
+                        {
+                            this.createCardList()
+                        }
                     </div>
                 </div>
             </Fragment>
